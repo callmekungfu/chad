3da0b70b0b06e06d5360bc8b75c4995b97a9cc48
Yong Lin Wang <wangyonglin1999@gmail.com>
2019-11-09 13:48:30 -0500

    removed example code

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,30 +1,25 @@
-import 'package:client/models/service.dart';
 import 'package:client/screens/service-management/service-management.dart';
 import 'package:flutter/material.dart';
-import 'package:client/blocprovs/example-block-prov.dart';
 import 'package:client/screens/login/login.dart';
-import 'package:client/screens/example2/examplescreen2.dart';
 import 'package:client/screens/admin/admin-screen.dart';
 import 'package:client/screens/serviceBrowser/service-browser-screen.dart';
 
 void main() {
-  runApp(ExampleApp());
+  runApp(ChadApp());
 }
-class ExampleApp extends StatelessWidget {
+class ChadApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ExampleProvider(
-      child: MaterialApp(
+    return 
+      MaterialApp(
         title: 'CHAD Clinic',
         initialRoute: '/serviceBrowser',
         routes: <String, WidgetBuilder>{
           "/": (BuildContext context) => LoginScreen(),
           "/serviceManagement": (BuildContext context) => ServiceManagementForm(),
-          "/ExScreen2": (BuildContext context) => ExScreen2(),
           "/admin": (BuildContext context) => AdminScreen(),
           "/serviceBrowser": (BuildContext context) => ServiceBrowserScreen(),
         },
-      ),
-    );
+      );
   }
 }
