886f468cfda3af506d823bd2db54f8f6e3cf08e2
Zoom1120 <33260391+Zoom1120@users.noreply.github.com>
2019-11-20 17:10:21 -0500

    Junit Tests

---
+++ client/test/service_provider_test.dart
@@ -0,0 +1,16 @@
+import 'package:client/screens/employee-hub-view/components/body.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Service Provider Profile Page', (){
+    var page = Body().createState();
+    test('get start date from combination', (){
+      const testCase = '11:30 - 12:30';
+      expect(page.startTime(testCase), '11:30');
+    });
+    test('get start date from combination', (){
+      const testCase = '11:30 - 12:30';
+      expect(page.endTime(testCase), '12:30');
+    });
+  });
+}
